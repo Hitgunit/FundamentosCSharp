1e0a6217dba3b73714039573a28b3515f4a4130f
Adair Duarte <ada_dp10@hotmail.com>
2023-05-20 13:46:45 -0600

    Se agrego la carpeta servicios y modifico el sendrequest

--- ConsoleApp1/Service/SendRequest.cs
+++ ConsoleApp1/Service/SendRequest.cs
@@ -1,46 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Text.Json;
-using System.Text.Json.Serialization;
-using Fundamentos.Models;
-using System.IO;
 using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace ConsoleApp1.Service
 {
-     class Program
+    //La "T" indica que va a recibir una clase con la cual va a trabajar
+    public class SendRequest<T>
     {
-        static async Task Main(string[] args)
+        private HttpClient _client = new HttpClient();
+        //Se crea una clase que funcionara con cualquier clase que reciba
+        public async Task<T> Send (T model)
         {
             string url = "https://jsonplaceholder.typicode.com/posts";
-            var client = new HttpClient();
 
-            Post post = new Post()
-            {
-                userId = 1,
-                body = "Hola soy un body",
-                tittle = "Hola soy un titulo"
-            };
             //Se convierte en un Json
-            var data = JsonSerializer.Serialize<Post>(post);
+            var data = JsonSerializer.Serialize<T>(model);
             //Se manda el objeto tipo Json, su formato encoding (acentos y asi) y el tipo que se mandara (Json en este caso)
             HttpContent content = new StringContent(data, System.Text.Encoding.UTF8, "application/json");
             //Varaible para obtener la respuesta (post, update and delete)
-            var httpResponse = await client.PostAsync(url, content);
+            var httpResponse = await _client.PostAsync(url, content);
             //Se valdia si fue exitoso
             if (httpResponse.IsSuccessStatusCode)
             {
                 //Se pasa el valro a una variable
                 var result = await httpResponse.Content.ReadAsStringAsync();
                 //Se deserializa para hacer match con los atributos
-                var postResult = JsonSerializer.Deserialize<Post>(result);
-                //Se lee cada atributo
-                Console.WriteLine(postResult.body);
+                var postResult = JsonSerializer.Deserialize<T>(result);
+                //si es exitoso regresa el postResult
+                return postResult;
             }
-            
+            //en caso de que falle se manda llamar a default
+            return default(T);
         }
     }
 }
